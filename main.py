63e3553d78a2853bb6d7aedd98c1aba3f14f0764
Pablo Pintor <48258972+MrCabss69@users.noreply.github.com>
2023-01-16 12:37:52 -0500

    Add files via upload
    checkpoint - code refactor & cleaning + fixed error

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import os
+import time
+import chess
+from engine import Engine
+from nodo import Nodo
+
+
+if __name__ == "__main__":
+
+    board = chess.Board()
+    print('\n', 'Board and starting position setted')
+    print(board)
+
+    while 1:
+        
+        cmd = input().split(" ")
+
+        if cmd[0] == "show":
+            print(board.legal_moves)
+            print(board)
+
+        elif cmd[0] == "go":
+            t, e  = time.time(), Engine()
+            move  = e.get_move(board)
+            try:
+                board.push(move)
+            except:
+                try:
+                    board.push(chess.Move.from_uci(move))
+                except:
+                    print('Try again')
+            
+            print(board)
+            print('Time to make the move: ', time.time()-t)
+            print('Analized positions: ',e.posCont)
+            
+        elif cmd[0] == 'move':
+            try:
+                board.push_san(cmd[1])
+                print(board)
+            except:
+                print('Wrong move format, try again')
+
+        elif cmd[0] == 'eval':
+            e = Engine()
+            print('Stockfish_val:', e.stockfish_value(Nodo(board)))
+            # print(heuristic_value(Nodo(board)))
+        
+        elif cmd[0] == 'clear':
+            os.system('cls' if os.name == 'nt' else 'clear')
--- nodo.py
+++ nodo.py
@@ -1,9 +1,4 @@
-import copy
 import chess
-
-v = {'p': -1, 'P': 1, 'n': -3, 'N': 3, 'b': -3, 'B': 3, 'k': -
-     1000, 'K': 1000, 'r': -5, 'R': 5, 'q': -9, 'Q': 9}
-
 
 class Nodo():
 
@@ -11,20 +6,21 @@ class Nodo():
         self.visits = 1
         self.board = board
         self.fen = board.fen()
-        self.terminal = self.board.outcome() != None
+        self.child_moves, self.childs = self.get_childs()
+        self.terminal = self.board.is_game_over()
         if self.terminal:
             res = self.board.outcome().result()
 
+    # get_childs -> return lista de string con FEN's de posiciones legales a un movimiento
     def get_childs(self):
-        board = copy.deepcopy(self.board)
-        childs = {}
-        for m in board.legal_moves:
-            board.push(m)
-            hijo = Nodo(chess.Board(board.fen()))
-            childs[m.uci()] = hijo
-            board.pop()
-        self.childs = childs  # legal moves productions
-        return childs
+        childs = []
+        moves  = []
+        for m in self.board.legal_moves:
+            self.board.push(m)
+            childs.append(self.board.fen())
+            moves.append(m)
+            self.board.pop()
+        return moves,childs
 
     def update_visit(self):
         self.visits += 1
