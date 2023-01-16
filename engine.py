f7e6d2715ad766f30da9d4d42e6bb5ef45f57ae4
Pablo Pintor <48258972+MrCabss69@users.noreply.github.com>
2023-01-16 18:12:18 -0500

    Add files via upload

--- engine.py
+++ engine.py
@@ -1,16 +1,18 @@
-import chess 
+import chess
 import random
 from stockfish import Stockfish
 
+
 class Engine():
 
+    # constructor
     def __init__(self) -> None:
         self.eng = Stockfish()
         self.ter = {}
         self.posCont = 0
 
     # get_random_board -> devuelve una posición aleatoria de un tablero de ajedrez
-    def get_random_board(self,n_moves=None):
+    def get_random_board(self, n_moves=None):
         if n_moves == None:
             n_moves = random.randint(100, 125)
         board = chess.Board()
@@ -22,10 +24,12 @@ class Engine():
             n_moves -= 1
         return board
 
+    # stockfish_value -> devuelve la evaluación de stockfish para un bord dado em la forma (best_move,board_eval)
+
     def stockfish_value(self, board):
 
         self.eng.set_fen_position(board.fen())
-        eval   = self.eng.get_evaluation()
+        eval = self.eng.get_evaluation()
         best_m = self.eng.get_best_move()
 
         if board.is_game_over() and board.fen() not in self.ter:
@@ -37,6 +41,7 @@ class Engine():
         elif eval["type"] == "mate":
             return best_m, 100_000 if eval['value'] > 0 else best_m, -100_000
 
+    # get_material_v -> devuelve en un valor numérico el desequilibrio material
 
     def get_material_v(self, board):
         v = 0
@@ -48,11 +53,12 @@ class Engine():
                     v += piece_value
                 else:
                     v -= piece_value
-        return v 
+        return v
 
+    # heuristic_value -> devuelve un valor heurístico para un nodo
 
     def heuristic_value(self, board):
-        # el tablero ya es terminal
+        # tablero terminal
         if board.is_game_over():
             res = board.outcome().result()
             if '/' in res:
@@ -63,32 +69,68 @@ class Engine():
         else:
             return self.get_material_v(board)
 
-    def get_childs(self,board):
-        hijos  = []
-        for m in board.legal_moves:
+    # sort_moves -> devuelve un valor que evalúa cuán de bueno es un movimiento 'a primeras'
+    def rate_move(self, board, move):
+        if not board.is_valid():
+            return -100
+        v = 0.
+        ov = self.heuristic_value(board)
+        
+        board.push(move)
+        if not board.is_valid():
+            return -100
+        # aplicar heurística de jaques 
+        if board.is_check():
+            v += .5
+        # aplicar heurística de material
+        nv              = self.heuristic_value(board)
+        piece_moved     = board.piece_at(move.from_square)
+        if piece_moved:
+            piece_moved_val = chess.PIECE_TYPES[piece_moved.piece_type-1]
+            if nv > ov:
+                # aplicar heurística capturas con política mvp-lvp
+                # PSEUDO MVP-LVP: cuanto más vale la pieza con la que capturamos, menos vale la captura
+                v += abs(nv-ov) * ( 3 / piece_moved_val )
+            elif nv < ov:
+                v -= abs(nv-ov) * ( 3 / piece_moved_val )
+        board.pop()
+        return v
+
+    def get_childs(self, board):
+
+        to_explore = list(board.legal_moves)
+        to_explore.sort(key=lambda x: self.rate_move(board,x))
+        moves = []
+        # hijos = []
+        for m in to_explore:
             board.push(m)
-            hijos.append(board.fen())
+            if board.is_valid():
+                moves.append(m)
+                #hijos.append(board.fen())
             board.pop()
-        return hijos
+        return moves
 
     # alfa-beta prunning + memoization - LONG + FAST VERSION
     def minimax_ab(self, board, depth, maximize, alfa, beta):
         self.posCont += 1
         # check terminal o maxima alcanzada del minimax por ahora
         if board.is_game_over() or depth == 0 or board.fen in self.ter.keys():
-            
+
             if board.fen() not in self.ter.keys():
                 self.ter[board.fen()] = self.heuristic_value(board)
                 # self.nodos[node.fen] = self.stockfish_value(node)[1]
             return self.ter[board.fen()]
-            
+
         elif maximize:
             # estamos maximizando - actualizamos alfa
             act = -100_000
             for f in self.get_childs(board):
-                hijo = chess.Board(f)
-                act  = max(act, self.minimax_ab(hijo, depth-1, False, alfa, beta))
-                # cortamos cuando nuestro valor a_maximizar, sea mayor que el del minimizador - él siempre escogera este
+                board.push(f)
+                act = max(act, self.minimax_ab(
+                    board, depth-1, False, alfa, beta))
+                board.pop()
+                # cortamos cuando nuestro valor_a_maximizar sea mayor que el del minimizador
+                # el adversario (mimizador) siempre escogera el menor en su turno
                 if act >= beta:
                     break
                 alfa = max(alfa, act)
@@ -97,33 +139,36 @@ class Engine():
             # estamos minimizando - actualizamos alfa
             act = 100_000
             for f in self.get_childs(board):
-                hijo = chess.Board(f)
-                act = min(act, self.minimax_ab(hijo, depth-1, True, alfa, beta))
+                board.push(f)
+                act = min(act, self.minimax_ab(
+                    board, depth-1, True, alfa, beta))
+                # cortamos cuando nuestro valor_a_minimizar sea menor que el del adversario
+                # él como maximizador siempre escogera el mayor de los valores en su turno
+                board.pop()
                 if act <= alfa:
                     break
                 beta = min(beta, act)
             return act
 
+    def get_move(self, board, depth=2, limit=None):
 
-    def get_move(self, board, depth=3,limit=None):
-         
         # TODO: IMPLEMENTAR estructura <GAMETREE> (para memoization) - almacenar los nodos recorridos y sus valores
         #   - ir recalculando recursivamente las últimas hojas candidatas según aumentemos la profundidad
+        # TABLA DE TRANSPOSICIONES CON NOTACIÓN ZOIBRIST
 
-        maximize   = bool(board.turn)
-        comparator = " < " if maximize else  " > " 
+        maximize = bool(board.turn)
+        comparator = " < " if maximize else " > "
 
-        best_move  = None
-        best_val   = -100_000 if maximize else 100_000
+        best_move = None
+        best_val = -100_000 if maximize else 100_000
 
         # recorremos los hijos del nodo actual
         for m in board.legal_moves:
             board.push(m)
-            mmval = self.minimax_ab(board, depth, not maximize, -100_000, 100_000)
+            mmval = self.minimax_ab(
+                board, depth, not maximize, -100_000, 100_000)
             # si es un nodo mejor que el actual
             if eval(str(best_val)+comparator+str(mmval)) == True:
                 best_val, best_move = mmval, m
             board.pop()
-
-        print(best_move)
-        return best_move, best_val# if move != None else random.choice(list(board.legal_moves))
+        return best_move, best_val
--- test.py
+++ test.py
@@ -3,35 +3,43 @@
 # IMPORTS
 from engine import Engine
 import pandas as pd
+import random
 import copy
 import time
 
+
+# CONSTANTS
+
+# victory heuristic value
+C = 100_000
 # timestamp
 ts = time.time()
 
-
+"""
 def get_openings():
     df = pd.read_csv('./resources/openings.csv',index_col=0)
     df = df.loc[:, ['moves']]
     df = df.applymap(lambda x: x.split(" "))
     print(df)
-# get_openings()
+get_openings()
+"""
 
        
 engine = Engine()
 nodos_terminales, posCont, v = {}, 0, -1 
-while abs(v) != 100_000: 
+while abs(v) > 0.9*C: 
     rand_board = engine.get_random_board(100)
     while rand_board.outcome() != None:
-        rand_board = engine.get_random_board(150)
-    m, v  = engine.get_move(rand_board,1)
-    if abs(v) == 100_000:
+        rand_board = engine.get_random_board(100)
+    m, v  = engine.get_move(rand_board, 3)
+    if abs(v) > 0.9*C:
         post_board = copy.deepcopy(rand_board)
         post_board.push(m)
         break
 
 
-print('Board escogido; ','\n', rand_board,'\n')
-print('Numero de posiciones recorridas: ', engine.posCont,'\n')
-print('Movimiento escogido: ', m,'\n')
-print('Board de jaque mate: ','\n', post_board)
+print('Board escogido: ', '\n', rand_board, '\n')
+print('Fen inicial: ', rand_board.fen(), '\n')
+print('Movimiento escogido: ', m, '\n')
+print('Numero de posiciones recorridas: ', engine.posCont, '\n')
+print('Tiempo de ejecución: ', str(time.time()-ts))
